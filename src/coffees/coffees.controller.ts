7ef01f2ffac78bf4ac16e9dfe3796189a2eb2b84
Dika Aditia <aditiaaadicka@gmail.com>
2023-01-08 20:29:26 +0700

    move logic to services, add friendly error message

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,29 +1,32 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(private readonly coffeeService: CoffeesService){}
+
     @Get()
-    findAll(){
-        return 'abc'
+    findAll(@Query() paginationQuery){
+        return this.coffeeService.findAll()
     }
 
     @Get(':id')
     findOne(@Param('id') id: string){
-        return `returning id #${id} coffees`
+        return this.coffeeService.findOne(id)
     }
 
     @Post()
     create(@Body() body) {
-        return body;
+        return this.coffeeService.create(body)
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body){
-        return `update returning id #${id} coffees`
+        return this.coffeeService.update(id, body)
     }
 
     @Delete(':id')
     remove(@Param('id') id: string){
-        return `delete returning id #${id} coffees`
+        return this.coffeeService.remove(id)
     }
 }
