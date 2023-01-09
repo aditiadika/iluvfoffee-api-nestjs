bf0b9d959a5547ac65d2fcab61b7c5a71d3ccd32
dika.aditia <dika.aditia@intidata.net>
2023-01-09 16:18:14 +0700

    handling malicious request data

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from "class-validator"
+
 export class UpdateCoffeeDto {
+    @IsString()
     readonly name?: string
+
+    @IsString()
     readonly brand?: string
+
+    @IsString()
     readonly flavours?: string[]
 }
