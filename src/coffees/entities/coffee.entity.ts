4d04470fa8bdd240d8541417c8d13e06fd904a2e
dika.aditia <dika.aditia@intidata.net>
2023-01-13 09:14:00 +0700

    create relations entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { type } from "os"
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm"
+import { Flavour } from "./flavour.entity"
 
 @Entity()
 export class Coffee {
@@ -11,6 +13,10 @@ export class Coffee {
     @Column()
     brand: string
 
-    @Column('json', { nullable: true })
+    @JoinTable()
+    @ManyToMany(
+        type => Flavour,
+        flavour => flavour.coffees
+    )
     flavours: string[]
 }
