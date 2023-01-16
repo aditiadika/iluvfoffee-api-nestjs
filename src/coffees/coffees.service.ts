6a748f2e12d619bd040a68fb0ca8cec916adb78a
dika.aditia <dika.aditia@intidata.net>
2023-01-16 08:25:36 +0700

    adding relation to cofee service, wip on cascading insert and update method

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,20 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { Flavour } from './entities/flavour.entity';
 
 @Injectable()
 export class CoffeesService {
     constructor(
         @InjectRepository(Coffee)
-        private readonly coffeeRepository: Repository<Coffee>
+        @InjectRepository(Flavour)
+
+        private readonly coffeeRepository: Repository<Coffee>,
+        private readonly flavourRepository: Repository<Coffee>
     ){}
 
     findAll(){
-        return this.coffeeRepository.find();
+        return this.coffeeRepository.find({
+            relations: ['flavours']
+        });
     }
 
     async findOne(id: string){
-        const coffee = await this.coffeeRepository.findOneBy({id: parseInt(id)})
+        const coffee = await this.coffeeRepository.findOne({
+            where: {id: parseInt(id)},
+            relations: ['flavours']
+        })
         if (!coffee) {
             throw new NotFoundException(`Coffee #${id} not found.,`)
         }
@@ -43,5 +52,13 @@ export class CoffeesService {
     async remove(id: string){
         const coffee = await this.coffeeRepository.findOneBy({id: parseInt(id)})
         return this.coffeeRepository.remove(coffee)
+    }
+
+    private async preloadFlavourByName(name: string): Promise<Flavour>{
+        const existingFlavours = await this.flavourRepository.findOneBy({ name })
+        if(existingFlavours) {
+            return existingFlavours;
+        }
+        return this.flavourRepository.create({ name })
     }
 }
